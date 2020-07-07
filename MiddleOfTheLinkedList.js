bb52f1cf346d2dc945ea7925ba15aa9bc2c967b5
Mark Lee <markcmlee@gmail.com>
2020-07-07 18:28:41 -0400

    saved some files

--- MiddleOfTheLinkedList.js
+++ MiddleOfTheLinkedList.js
@@ -1,17 +1,5 @@
 // https://leetcode.com/problems/middle-of-the-linked-list/
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-
 const middleNode = (head) => {
   let counter = 0;
   let current = head;
--- MoveZeroes.js
+++ MoveZeroes.js
@@ -1,27 +1,18 @@
 // https://leetcode.com/problems/move-zeroes/submissions/
 
-// const moveZeroes = nums => {
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[i] === 0) {
-//       nums.push(Number(nums.splice(i, 1).toString()));
-//     }
-//   }
-//   return nums;
-// };
-
 const moveZeroes = (nums) => {
+  let pointer = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === 0) {
-      nums.push(Number(nums.splice(i, 1).toString()));
-    }
-  }
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === 0) {
-      nums.push(Number(nums.splice(i, 1).toString()));
+    if (nums[i] !== 0) {
+      if (i !== pointer) {
+        nums[pointer] = nums[i];
+        nums[i] = 0;
+      }
+      pointer++;
     }
   }
   return nums;
 };
 
-// console.log(moveZeroes([0, 1, 0, 3, 12]));
+console.log(moveZeroes([0, 1, 0, 3, 12]));
 console.log(moveZeroes([0, 0, 1]));
