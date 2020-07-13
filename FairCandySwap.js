6e530b3859095a376a3041d5db77e720de2f196e
Mark Lee <markcmlee@gmail.com>
2020-07-13 16:13:07 -0400

    fairCandySwap

---
+++ FairCandySwap.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/fair-candy-swap/
+
+const fairCandySwap = (A, B) => {
+  for (let i = 0; i < A.length; i++) {
+    const aSum = A.reduce((a, b) => a + b);
+    const bSum = B.reduce((a, b) => a + b);
+    for (let j = 0; j < B.length; j++) {
+      if (A[i] === (aSum - bSum) / 2 + B[j]) {
+        return [A[i], B[j]];
+      }
+    }
+  }
+};
+
+console.log(fairCandySwap([1, 1], [2, 2]));
+console.log(fairCandySwap([1, 2], [2, 3]));
+console.log(fairCandySwap([2], [1, 3]));
+console.log(fairCandySwap([1, 2, 5], [2, 4]));
