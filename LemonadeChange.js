ffb415873db191f31bdd03e71af886eccd180346
Mark Lee <markcmlee@gmail.com>
2020-07-13 17:27:08 -0400

    changed lemonade

--- LemonadeChange.js
+++ LemonadeChange.js
@@ -1,25 +1,5 @@
 // https://leetcode.com/problems/lemonade-change/
 
-// const lemonadeChange = (bills) => {
-//   const tally = {};
-//   bills.forEach((bill) => {
-//     tally[bill] ? (tally[bill] += 1) : (tally[bill] = 1);
-//   });
-//   for (let i = 0; i < bills.length; i++) {
-//     if (bills[i] === 10) {
-//       tally[5] -= 1;
-//     } else if (bills[i] === 20) {
-//       if (tally[10] >= 1 && tally[5] >= 1) {
-//         tally[10] -= 1;
-//         tally[5] -= 1;
-//       } else {
-//         tally[5] -= 3;
-//       }
-//     }
-//   }
-//   return tally[5] >= 0;
-// };
-
 const lemonadeChange = (bills) => {
   const tally = {};
   for (let i = 0; i < bills.length; i++) {
@@ -47,3 +27,6 @@ console.log(lemonadeChange([10, 10])); // false
 console.log(lemonadeChange([5, 5, 10, 10, 20])); // false
 console.log(lemonadeChange([5, 5, 5, 5, 10, 5, 10, 10, 10, 20])); // true
 console.log(lemonadeChange([5, 5, 5, 5, 20, 20, 5, 5, 5, 5])); // false
+
+const obj = { test: 0 };
+console.log(!obj["test"]);
