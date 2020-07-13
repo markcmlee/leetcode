78695987cdbfac2c7d797427268a548e40432f97
Mark Lee <markcmlee@gmail.com>
2020-07-13 15:12:55 -0400

    saved changes

--- FindAllNumbersDisappearedFromAnArray.js
+++ FindAllNumbersDisappearedFromAnArray.js
@@ -1,17 +1,16 @@
 // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
 const findDisappearedNumbers = (nums) => {
-  const sorted = nums.sort((a, b) => a - b);
-  console.log(sorted);
   const result = [];
-  for (let i = 1; i < sorted.length; i++) {
-    if (sorted[i] !== i) {
-      if (sorted[i] !== sorted[i - 1] && sorted[i - 1] !== undefined) {
-        result.push(i);
-      }
+  const unique = new Set(nums);
+  for (let i = 1; i <= nums.length; i++) {
+    if (!unique.has(i)) {
+      result.push(i);
     }
   }
   return result;
 };
 
 console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
+
+console.log(findDisappearedNumbers([1, 1]));
