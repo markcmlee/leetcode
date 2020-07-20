0ec06f8074866426d7f176763fe5fbc425ff73de
Mark Lee <markcmlee@gmail.com>
2020-07-20 17:44:59 -0400

    minTimeToVisitAllPoints

---
+++ MinTimeToAllPoints.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/minimum-time-visiting-all-points/
+
+const minTimeToVisitAllPoints = (points) => {
+  if (points.length < 2) return 0;
+  let counter = 0;
+  for (let i = 1; i < points.length; i++) {
+    const x = Math.abs(points[i - 1][0] - points[i][0]);
+    const y = Math.abs(points[i - 1][1] - points[i][1]);
+    time += Math.max(x, y);
+  }
+  return counter;
+};
+
+console.log(
+  minTimeToVisitAllPoints([
+    [1, 1],
+    [3, 4],
+    [-1, 0],
+  ])
+); //7
+
+console.log(
+  minTimeToVisitAllPoints([
+    [3, 2],
+    [-2, 2],
+  ])
+); //5
